8fc2fed4e1f72e91bd19fd84dbb61cfa052c1644
c4lyp5o <calypso@calypsocloud.one>
2025-05-05 01:01:49 +0800

    feat: added vitest config

---
+++ vitest.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		// Enable coverage reporting
+		coverage: {
+			provider: "v8", // Use 'v8' or 'c8' as the coverage provider
+			reporter: ["text", "lcov", "html"], // Generate coverage reports in text, lcov, and HTML formats
+			include: ["src/*.js", "src/utils/*.js"], // Include all source files in the `src` directory
+			exclude: ["node_modules", "test", "dist"], // Exclude test files, dependencies, and build files
+		},
+	},
+});
